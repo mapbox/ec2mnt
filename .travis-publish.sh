88f0259cb6b13f19353a5c2b27767074d7f46e76
Young Hahn <young@mapbox.com>
2016-01-09 12:57:23 -0500

    Omit the /run

--- .travis-publish.sh
+++ .travis-publish.sh
@@ -5,6 +5,6 @@ set -eu
 if git describe --tags --exact-match 2> /dev/null; then
     pip install awscli
     tag=$(git describe --tags --exact-match)
-    aws s3 cp --acl public-read "$(dirname $0)/bin/ec2mnt" s3://mapbox/apps/ec2mnt/$tag/run
+    aws s3 cp --acl public-read "$(dirname $0)/bin/ec2mnt" s3://mapbox/apps/ec2mnt/$tag
 fi
 
